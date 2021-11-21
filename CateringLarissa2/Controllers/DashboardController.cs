ba477032846937bd9f54d346ebbe4328dc8cc3d6
ThoriqHuda <hyokoshn@gmail.com>
2021-11-21 21:20:55 +0700

    Add project files.

---
+++ CateringLarissa2/Controllers/DashboardController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CateringLarissa2.Controllers
+{
+    public class Dashboard : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Review()
+        {
+            return View();
+        }
+
+        public IActionResult Account()
+        {
+            return View();
+        }
+    }
+}
---
+++ CateringLarissa2/Models/Review.cs
@@ -0,0 +1,16 @@
+﻿namespace CateringLarissa2.Models
+{
+    public class Review
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public string description { get; set; }
+        public string user { get; set; }
+        public int rating { get; set; }
+
+        public Review()
+        {
+
+        }
+    }
+}
